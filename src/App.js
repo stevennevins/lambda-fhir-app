176ac0796b9e7c1bbf399a6a74ca4e32fb19804e
steven <steven.nevins.js@gmail.com>
2020-12-20 15:09:07 -0500

    updated readmen and added graphql to lambda project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "/.netlify/functions/graphql"
+});
 
 class LambdaDemo extends Component {
   constructor(props) {
